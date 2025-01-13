7d10bff14b5c2867834983eaf931f487fece98c3
Jesús Martínez Sequeiros <165663442+Jesus2508@users.noreply.github.com>
2025-01-13 11:59:15 +0000

    cartas terminadas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,45 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const getRandomElement = array => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+window.onload = () => {
+  const suits = ["♥", "♦", "♠", "♣"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomSuit = getRandomElement(suits);
+  const randomValue = getRandomElement(values);
+
+  const cardElement = document.getElementById("card");
+  cardElement.innerHTML = `
+    <div class="top-suit">${randomSuit}</div>
+    <div class="value">${randomValue}</div>
+    <div class="bottom-suit">${randomSuit}</div>
+  `;
+
+  cardElement.classList.add(
+    randomSuit === "♥"
+      ? "heart"
+      : randomSuit === "♦"
+      ? "diamond"
+      : randomSuit === "♠"
+      ? "spade"
+      : "club"
+  );
 };
